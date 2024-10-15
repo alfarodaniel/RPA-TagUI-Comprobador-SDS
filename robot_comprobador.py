58b139d47aca7d87690c49146f65689d5ed558b7
alfarodaniel <alfaro_daniel@hotmail.com>
2024-10-14 19:10:41 -0500

    Robot busqueda eficiente

--- robot_comprobador.py
+++ robot_comprobador.py
@@ -25,20 +25,32 @@ for n, registro in enumerate(Comprobar, start=1):
     r.timeout(10)
 
     # Validar si existe el registro en el comprobador
-    if r.exist('//*[@id="MainContent_cmdNuevaConsulta"]') or r.exist('//*[@id="MainContent_lblError"]'):
-        if r.present('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[1]/th[14]'):
-            # Leer EPS-S
-            epss = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[14]')
+    for i in range(1, 11):
+        # Verifica si se encontró
+        if r.present('//*[@id="MainContent_cmdNuevaConsulta"]'):
+            break        # Verifica si se encontró
 
-            # Leer Estado
-            estado = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[16]')
-            print('-', epss, estado)
-            resultado = registro + ',' + epss + ',' + estado + '\n'
-            r.write(resultado, 'comprobados_sds.csv')
-        else:
-            print('- Sin información')
-            resultado = registro  + '\n'
-            r.write(resultado, 'no_comprobados.csv')
+        # Verifica si no se encontró
+        if r.present('//*[@id="MainContent_lblError"]'):
+            break
+        
+        # Esperar 1 segundo
+        r.wait(1)
+
+    # Verifica si se encontró
+    if r.present('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[1]/th[14]'):
+        # Leer EPS-S
+        epss = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[14]')
+
+        # Leer Estado
+        estado = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[16]')
+        print('-', epss, estado)
+        resultado = registro + ',' + epss + ',' + estado + '\n'
+        r.write(resultado, 'comprobados_sds.csv')
+    else:
+        print('- Sin información')
+        resultado = registro  + '\n'
+        r.write(resultado, 'no_comprobados.csv')
 
 # Cerrar robot
 print('Cerrar robot')
