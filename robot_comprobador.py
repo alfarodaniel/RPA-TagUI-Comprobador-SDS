fffa1bb1e6a097446d27e2437bc12f5b8c5031e8
Daniel Alfaro Rojas <alfaro_daniel@hotmail.com>
2024-10-16 11:02:12 -0500

    Version 3

--- robot_comprobador.py
+++ robot_comprobador.py
@@ -5,7 +5,7 @@ Buscar en el comprobador de derechos de la SDS las identificaciones listadas en 
 # Cargar librerías
 import rpa as r
 
-# Funcion para validar si existe el registro en el comprobador
+# Función para validar si existe el registro en el comprobador
 def esperar():
     for i in range(1, 11):
         # Verifica si se encontró
@@ -20,6 +20,68 @@ def esperar():
         
         # Esperar 1 segundo
         r.wait(1)
+
+# Función para generar registro cuando no se encontró información
+def sinInformacion(registro):
+    print('- Sin información')
+    resultado = registro  + '\n'
+    r.write(resultado, 'no_comprobados.csv')
+
+# Función para generar registro cuando se encontró información
+def conInformacion(registro, sesion):
+    # Valida si se inició sesión
+    if sesion:
+        epsscol = '15'
+        estadocol = '18'
+    else:
+        epsscol = '14'
+        estadocol = '16'
+    
+    # Verifica primer apellido
+    # Tabla con celda 'Primer Apellido' fila 2
+    if r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[' + epsscol + ']') == registro.split(',')[3]:
+        # Leer EPS-S
+        # Tabla con celda 'EPS-S' fila 2
+        epss = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[' + epsscol + ']')
+
+        # Leer Estado
+        # Tabla con celda 'Estado' fila 2
+        estado = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[' + estadocol + ']')
+        print('-', epss, estado)
+        resultado = registro + ',' + epss + ',' + estado + '\n'
+        r.write(resultado, 'comprobados_sds.csv')
+        return(True)
+    else:
+        # Valida si se inició sesión para no generar doble registro
+        if sesion == False:
+            sinInformacion(registro)
+        return(False)
+
+# Función para validar si existe el registro en el comprobador por nombres
+def comprobadorNombres(registro):
+    # Abrir comprobador
+    r.url('https://appb.saludcapital.gov.co/Comprobadordederechos/Consulta.aspx')
+
+    # Buscar por nombres
+    # Celda 'Primer Apellido'
+    r.type('//*[@id="MainContent_txtPriApellido"]', '[clear]' + registro.split(',')[3])
+    # Celda 'Segundo Apellido'
+    r.type('//*[@id="MainContent_txtSegApellido"]', '[clear]' + registro.split(',')[4])
+    # Celda 'Primer Nombre'
+    r.type('//*[@id="MainContent_txtPriNombre"]', '[clear]' + registro.split(',')[1])
+    # Celda 'Segundo Nombre'
+    r.type('//*[@id="MainContent_txtSegNombre"]', '[clear]' + registro.split(',')[2])
+    # Botón 'Consultar'
+    r.click('//*[@id="MainContent_cmdConsultar"]')
+    
+    # validar si existe el registro en el comprobador
+    esperar()
+
+    # Verifica si se encontró
+    if r.present('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[1]/th[15]'):
+        conInformacion(registro, True)
+    else:
+        sinInformacion(registro)
 
 # Cargar archivo
 print('Cargar archivo comprobar.csv')
@@ -68,78 +130,16 @@ for n, registro in enumerate(Comprobar, start=1):
         # Verifica si se encontró
         # Tabla con celda 'EPS-S' fila 1
         if r.present('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[1]/th[15]'):
-            # Verifica primer apellido
-            # Tabla con celda 'Primer Apellido' fila 2
-            if r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[15]') == registro.split(',')[3]:
-                # Leer EPS-S
-                # Tabla con celda 'EPS-S' fila 2
-                epss = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[15]')
-
-                # Leer Estado
-                # Tabla con celda 'Estado' fila 2
-                estado = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[18]')
-                print('-', epss, estado)
-                resultado = registro + ',' + epss + ',' + estado + '\n'
-                r.write(resultado, 'comprobados_sds.csv')
-            else:
-                print('- Sin información')
-                resultado = registro  + '\n'
-                r.write(resultado, 'no_comprobados.csv')
+            if conInformacion(registro, True) == False:
+                comprobadorNombres(registro)
         else:
-            # Validar si existe el registro en el comprobador por nombres
-
-            # Abrir comprobador
-            r.url('https://appb.saludcapital.gov.co/Comprobadordederechos/Consulta.aspx')
-
-            # Buscar por nombres
-            # Celda 'Primer Apellido'
-            r.type('//*[@id="MainContent_txtPriApellido"]', '[clear]' + registro.split(',')[3])
-            # Celda 'Segundo Apellido'
-            r.type('//*[@id="MainContent_txtSegApellido"]', '[clear]' + registro.split(',')[4])
-            # Celda 'Primer Nombre'
-            r.type('//*[@id="MainContent_txtPriNombre"]', '[clear]' + registro.split(',')[1])
-            # Celda 'Segundo Nombre'
-            r.type('//*[@id="MainContent_txtSegNombre"]', '[clear]' + registro.split(',')[2])
-            # Botón 'Consultar'
-            r.click('//*[@id="MainContent_cmdConsultar"]')
-            
-            # validar si existe el registro en el comprobador
-            esperar()
-
-            # Verifica si se encontró
-            if r.present('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[1]/th[15]'):
-                # Verifica primer apellido
-                # Tabla con celda 'Primer Apellido' fila 2
-                if r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[15]') == registro.split(',')[3]:
-                    # Leer EPS-S
-                    # Tabla con celda 'EPS-S' fila 2
-                    epss = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[15]')
-
-                    # Leer Estado
-                    # Tabla con celda 'Estado' fila 2
-                    estado = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[18]')
-                    print('-', epss, estado)
-                    resultado = registro + ',' + epss + ',' + estado + '\n'
-                    r.write(resultado, 'comprobados_sds.csv')
-                else:
-                    print('- Sin información')
-                    resultado = registro  + '\n'
-                    r.write(resultado, 'no_comprobados.csv')
+            comprobadorNombres(registro)
     else:
         # Verifica si se encontró
         if r.present('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[1]/th[14]'):
-            # Leer EPS-S
-            epss = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[14]')
-
-            # Leer Estado
-            estado = r.read('//*[@id="MainContent_grdSubsidiado"]/tbody/tr[2]/td[16]')
-            print('-', epss, estado)
-            resultado = registro + ',' + epss + ',' + estado + '\n'
-            r.write(resultado, 'comprobados_sds.csv')
+            conInformacion(registro, False)
         else:
-            print('- Sin información')
-            resultado = registro  + '\n'
-            r.write(resultado, 'no_comprobados.csv')
+            sinInformacion(registro)
 
 # Cerrar robot
 print('Cerrar robot')
